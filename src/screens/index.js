8cf2c2fc78b009a9638a16f40ecde0310e527fce
ADMIN <21522704@gm.uit.edu.vn>
2024-10-23 21:38:05 +0700

    Auth

---
+++ src/screens/index.js
@@ -0,0 +1,24 @@
+//Auth
+import Login from "./Auth/Login";
+import Registration from "./Auth/Registration";
+import ForgetPassword from "./Auth/ForgetPassword";
+import Verification from "./Auth/Verification";
+import ResetPassword from "./Auth/ResetPassword";
+
+//UserProfile
+import UserProfile from "./UserProfile/UserProfile";
+import ResetPasswordInProfile from "./UserProfile/ResetPasswordInProfile";
+import SettingProfile from "./UserProfile/SettingProfile";
+
+
+export {
+  Login,
+  Registration,
+  ForgetPassword,
+  Verification,
+  ResetPassword,
+  //
+  UserProfile,
+  ResetPasswordInProfile,
+  SettingProfile,
+};
